d6e3bac22d2b799bc6e18b06e313430d2edadc6f
Vaibhav <vaibhavverma21122001@gmai.com>
2024-02-06 01:20:48 +0530

    more changes

--- Node js Scalar/02_npm/index.js
+++ Node js Scalar/02_npm/index.js
@@ -1,7 +1,6 @@
 var figlet = require("figlet");
 
 
-
 figlet("Vaibhav!!", function (err, data) {
     if (err) {
         console.log("Something went wrong...");
@@ -9,3 +8,14 @@ figlet("Vaibhav!!", function (err, data) {
     }
     console.log(data);
 });
+
+
+// SEMANTIC VERSIONING 
+// eg - "figlet": "^1.7.0" (from package.json)
+
+// 1 - signifies major release -> when there are a lot of changes (huge changes)
+// 7 - signifies minor release -> some minor features are released
+// 0 - signifies patch release -> bugs fixes
+
+
+// ^ (carrot character) -> signifies while npm i then this particular version will be downloaded not the latest one
